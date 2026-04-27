04031bd97486170b1ccf5898e2de13d1d50534b5
Alagu veera siranjivee <163497543+siranjivee123@users.noreply.github.com>
2026-04-27 19:20:30 +0530

    Add employee and project routes to app.routes.ts

--- app.routes.ts
+++ app.routes.ts
@@ -1,10 +1,32 @@
-// app.routes.ts
 import { Routes } from '@angular/router';
+
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import { AddEmployeeComponent } from './add-employee/add-employee';
+import { EmployeeListComponent } from './employee-list/employee-list';
+
+//  Import Project Components
+import { ProjectsComponent } from './projects/projects';
+import { AddProjectComponent } from './add-projects/add-projects';
+
 export const routes: Routes = [
+
+  // Default redirect
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  // Auth
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent }
+
+  // Dashboard
+  { path: 'dashboard', component: DashboardComponent },
+
+  // EMPLOYEE ROUTES
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'add-employee', component: AddEmployeeComponent },
+
+  // PROJECT ROUTES 
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'add-project', component: AddProjectComponent },
+  { path: '**', redirectTo: 'login' }
 ];
