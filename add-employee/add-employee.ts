77896e2f4db9c4aa6c03301ea0e51eadea1a90f6
Alagu veera siranjivee <163497543+siranjivee123@users.noreply.github.com>
2026-04-30 16:50:11 +0530

    Enhance employee form with stepper and dialog
    Refactor employee form to use reactive forms with stepper. Add success dialog upon employee addition.

--- add-employee/add-employee.ts
+++ add-employee/add-employee.ts
@@ -2,10 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import Swal from 'sweetalert2';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { MatDialog } from '@angular/material/dialog';
 
-//MAterial imports:
+import { EmployeeSuccessDialogComponent } from './employee-success-dialog/employee-success-dialog.component';
+
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
@@ -16,6 +20,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     CommonModule,
     ReactiveFormsModule,
     HttpClientModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatInputModule,
     MatSelectModule,
     MatFormFieldModule
   ],
@@ -23,6 +30,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   styleUrls: ['./add-employee.css']
 })
 export class AddEmployeeComponent implements OnInit {
+
+  basicForm!: FormGroup;
+  techForm!: FormGroup;
 
   states: any[] = [];
   cities: any[] = [];
@@ -36,27 +46,115 @@ export class AddEmployeeComponent implements OnInit {
     'Profile page'
   ];
 
-  managersList: string[] = [
-    'Arun',
-    'Vinoth',
-    'Rahul',
-    'Ram'
-  ];
-
-  employeeForm!: FormGroup;
+  managersList: string[] = ['Arun', 'Vinoth', 'Rahul', 'Ram'];
 
   isLoadingCities = false;
   cityError = '';
-  private cityTimeout: any;
+  employeeId = '';
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private http: HttpClient
+    private http: HttpClient,
+    private dialog: MatDialog
   ) {}
 
-  // AUTO GENERATE ID:
+  ngOnInit() {
 
+// STEP 1 FORM
+    this.basicForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      address: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zip: ['', [Validators.required, Validators.pattern('^[0-9]{6}$')]],
+    });
+
+// STEP 2 FORM
+    this.techForm = this.fb.group({
+      role: ['', Validators.required],
+      shift: ['', Validators.required],
+      projects: [[], Validators.required],
+      managers: [[], Validators.required],
+    });
+
+// LOAD STATES
+    this.http.get<any>('https://countriesnow.space/api/v0.1/countries/states')
+      .subscribe(res => {
+        const india = res.data.find((c: any) => c.name === 'India');
+        this.states = india?.states || [];
+      });
+  }
+
+// NEXT STEP VALIDATION
+  goNext(stepper: any) {
+
+    if (this.basicForm.invalid) {
+      this.basicForm.markAllAsTouched();
+      return;
+    }
+
+    stepper.next();
+  }
+
+// STATE to  CITY
+  onStateChange() {
+
+    const selectedState = this.basicForm.get('state')?.value;
+
+    this.basicForm.patchValue({ city: '' });
+    this.cities = [];
+
+    if (!selectedState) return;
+
+    this.isLoadingCities = true;
+
+    this.http.post<any>(
+      'https://countriesnow.space/api/v0.1/countries/state/cities',
+      {
+        country: 'India',
+        state: selectedState
+      }
+    ).subscribe({
+      next: (res) => {
+        this.cities = res.data || [];
+        this.isLoadingCities = false;
+      },
+      error: () => {
+        this.cityError = 'Unable to load cities';
+        this.isLoadingCities = false;
+      }
+    });
+  }
+
+// SUBMIT
+  onSubmit() {
+
+    if (this.techForm.invalid) {
+      this.techForm.markAllAsTouched();
+      return;
+    }
+
+    const employees = JSON.parse(localStorage.getItem('employees') || '[]');
+
+    this.employeeId = this.generateId();
+
+    employees.unshift({
+      id: this.employeeId,
+      ...this.basicForm.value,
+      ...this.techForm.value,
+      date: new Date().toISOString(),
+      tasks: Math.floor(Math.random() * 100)
+    });
+
+    localStorage.setItem('employees', JSON.stringify(employees));
+
+    this.openSuccessDialog();
+  }
+
+// GENERATE ID
   generateId(): string {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     let id = '';
@@ -66,90 +164,27 @@ export class AddEmployeeComponent implements OnInit {
     return id;
   }
 
-  // INIT:
-  ngOnInit() {
+// POPUP
+  openSuccessDialog() {
 
-    this.employeeForm = this.fb.group({
-      id: [this.generateId()],
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      phone: ['', Validators.required],
-      address: ['', Validators.required],
-      city: ['', Validators.required],
-      state: ['', Validators.required],
-      zip: ['', Validators.required],
-      role: ['', Validators.required],
-      projects: [[], Validators.required],
-      managers: [[], Validators.required],
+    const dialogRef = this.dialog.open(EmployeeSuccessDialogComponent, {
+      width: '400px',
+      disableClose: true,
+      data: {
+        employeeId: this.employeeId
+      }
     });
 
-    this.http.get<any>('https://countriesnow.space/api/v0.1/countries/states')
-      .subscribe(res => {
-        const india = res.data.find((c: any) => c.name === 'India');
-        this.states = india?.states || [];
-      });
-  }
-
-  // STATE  CITIES:
-  onStateChange() {
-
-    clearTimeout(this.cityTimeout);
-
-    this.cityTimeout = setTimeout(() => {
-      const selectedState = this.employeeForm.get('state')?.value;
-      this.employeeForm.patchValue({ city: '' });
-      this.cities = [];
-      if (!selectedState) return;
-      this.isLoadingCities = true;
-      this.cityError = '';
-      this.http.post<any>(
-        'https://countriesnow.space/api/v0.1/countries/state/cities',
-        {
-          country: 'India',
-          state: selectedState
-        }
-      ).subscribe({
-        next: (res) => {
-          this.cities = res.data || [];
-          this.isLoadingCities = false;
-        },
-        error: () => {
-          this.cityError = 'Unable to load cities';
-          this.isLoadingCities = false;
-        }
-      });
-
-    }, 200);
-  }
-
-  // SUBMIT:
-
-  onSubmit() {
-    if (this.employeeForm.invalid) {
-      this.employeeForm.markAllAsTouched();
-      return;
-    }
-    const employees = JSON.parse(localStorage.getItem('employees') || '[]');
-    employees.unshift({
-      ...this.employeeForm.value,
-      date: new Date().toISOString(),
-      tasks: Math.floor(Math.random() * 100)
-    });
-    localStorage.setItem('employees', JSON.stringify(employees));
-    Swal.fire({
-      icon: 'success',
-      title: 'Employee Added!',
-      text: 'Saved successfully',
-      timer: 1500,
-      showConfirmButton: false
-    }).then(() => {
-      this.router.navigate(['/dashboard'], {
-        queryParams: { view: 'employees' }
-      });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === 'navigate') {
+        this.router.navigate(['/dashboard'], {
+          queryParams: { view: 'employees' }
+        });
+      }
     });
   }
-  // BACK:
-  goBack() {
+
+  goToList() {
     this.router.navigate(['/dashboard'], {
       queryParams: { view: 'employees' }
     });
